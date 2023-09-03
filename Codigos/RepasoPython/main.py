527aad147661aee2051202db2d6b54d2bc9a8704
zozimo <cristianzozimo.aranda@gmail.com>
2023-09-03 07:54:53 -0300

    Agrego la exportación de graficos en el notebook

--- Codigos/RepasoPython/main.py
+++ Codigos/RepasoPython/main.py
@@ -1,6 +1,5 @@
 import plotly.graph_objects as go
 import os
-
 if not os.path.exists("images"):
     os.mkdir("images")
 
@@ -62,11 +61,8 @@ fig.update_layout(legend_title_text='Termómetro',
                 
                 
                 )  
-
-ruta_archivo = os.path.join("images", "fig1.png")
-fig.write_image(ruta_archivo)
-fig.show()
-
+   
+fig.write_image("images/fig2.svg",format="svg")
 
 # # Guardar la figura como PDF
 # fig.write_image("Codigos\\RepasoPython\\image.pdf",format="pdf")
@@ -75,3 +71,36 @@ fig.show()
 
 # # Guardar la figura como SVG
 # fig.write_image("grafico.svg")
+
+
+# import plotly.graph_objects as go
+# import numpy as np
+# np.random.seed(1)
+
+# N = 100
+# x = np.random.rand(N)
+# y = np.random.rand(N)
+# colors = np.random.rand(N)
+# sz = np.random.rand(N) * 30
+
+# fig = go.Figure()
+# fig.add_trace(go.Scatter(
+#     x=x,
+#     y=y,
+#     mode="markers",
+#     marker=go.scatter.Marker(
+#         size=sz,
+#         color=colors,
+#         opacity=0.6,
+#         colorscale="Viridis"
+#     )
+# ))
+
+# fig.show()
+
+# import os
+
+# if not os.path.exists("images"):
+#     os.mkdir("images")
+    
+# fig.write_image("images/fig1.png")
