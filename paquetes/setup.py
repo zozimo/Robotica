6cbf020264f3c4ecc8b299db18e743f63279318a
zozimo <cristianzozimo.aranda@gmail.com>
2023-09-16 17:40:05 -0300

    Se sube un paquete que contiene funciones para las aplicaciones de Robotica

---
+++ paquetes/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name="recursos_robotica",
+    version="0.1",
+    description="Paquete con las funciones para trabajar los problemas de Robotica",
+    author="Cristian Zozimo Aranda Cordero",
+    author_email= "carandac@fi.uba.ar",
+    url="https://www.google.com",
+    packages=['recursos_robotica'],
+    scripts=[]   
+
+)
